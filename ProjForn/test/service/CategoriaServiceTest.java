4c5467cf3653cccc18261882e216293519736f3b
Felipe Galdino <felipegaldino16@gmail.com>
2019-06-11 19:58:08 -0300

    Finalizado testes unitarios

--- ProjForn/test/service/CategoriaServiceTest.java
+++ ProjForn/test/service/CategoriaServiceTest.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 import modelo.Categoria;
 import org.junit.After;
@@ -39,20 +35,65 @@ public class CategoriaServiceTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of getCategoriaByNome method, of class CategoriaService.
-     */
+    
+    //O Serviço da Categoria possui 4 métodos, sendo 3 herdados. Todos são testados abaixo.
+
     @Test
     public void testGetCategoriaByNome() {
         System.out.println("getCategoriaByNome");
         String value = "BEBIDAS";
         CategoriaService instance = new CategoriaService();
+        
         Categoria expResult = new Categoria("BEBIDAS");
         Categoria result = instance.getCategoriaByNome(value);
         assertEquals(expResult.getDescricao(), result.getDescricao());
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     
+    @Test
+    public void testGetAllCategoria() {
+        System.out.println("getAll");
+        CategoriaService instance = new CategoriaService();
+        List<Categoria> uneexpResult = new ArrayList<Categoria>();
+        List<Categoria> result = instance.getAll(Categoria.class);
+        
+        //Verifica se não retornou nulo e se não retornou vazio.
+        //Não foi utilizado assertEquals pois o banco é dinâmico e não seria bom comparar com algo.
+        assertNotNull(result);
+        assertNotEquals(uneexpResult, result);
+    }
     
+    @Test
+    public void testSalvarCategoria() {
+        
+        System.out.println("salvarCategoria");
+        CategoriaService instance = new CategoriaService();
+        Categoria obj = new Categoria("UTILIDADES");
+        
+        //Retorna 0 em caso de sucesso ou -1 em caso de falha.
+        int resultado = instance.salvar(obj);
+        assertEquals(resultado, 0);
+        
+        //Verifica se foi cadastrado com sucesso
+        String expResult = "UTILIDADES";
+        String result = instance.getCategoriaByNome(expResult).getDescricao();
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testRemoverCategoria() {
+        System.out.println("removerCategoria");
+        CategoriaService instance = new CategoriaService();
+        Categoria obj = new Categoria("FRUTAS");
+        instance.salvar(obj);
+        
+        //VErifica se retornou sucesso (0).
+        int resultado = instance.remover(obj);
+        assertEquals(resultado, 0);
+        
+        //Verifica se o objeto não existe mais
+        String nome = "FRUTAS";
+        Categoria result = instance.getCategoriaByNome(nome);
+        assertNotNull(result);
+    }
+        
 }
